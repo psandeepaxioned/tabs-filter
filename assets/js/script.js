36d239085e552daf1226c47deb1c8b2ed7a5836b
psandeepaxioned <vikasb@axioned.com>
2022-07-19 10:27:29 +0530

    scripting for the task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,52 @@
 
 */
 
+/* Author: 
+
+*/
+const types = document.querySelectorAll('.types');
+const images = document.querySelectorAll('.images');
+
+for(let i=0; i<types.length;i++){
+    types[i].addEventListener('click',function(){
+        for (j=0;j<types.length;j++){
+            types[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        const dataFilter = this.getAttribute('data-filter')
+
+        for(let k=0; k<images.length;k++){
+            images[k].classList.remove('active');
+            images[k].classList.add('hide');
+
+            if(images[k].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+                images[k].classList.remove('hide');
+                images[k].classList.add('active');
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
