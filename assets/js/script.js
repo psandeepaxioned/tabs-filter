46297650b70f952ac03c4ce7752cc5390976be0a
psandeepaxioned <vikasb@axioned.com>
2022-07-19 13:19:51 +0530

    scripting for the task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,29 @@
 /* Author: 
 
 */
-const types = document.querySelectorAll('.types');
-const images = document.querySelectorAll('.images');
+const types = document.querySelectorAll(".types");
+const images = document.querySelectorAll(".images");
 
-for(let i=0; i<types.length;i++){
-    types[i].addEventListener('click',function(){
-        for (j=0;j<types.length;j++){
-            types[j].classList.remove('active');
-        }
-        this.classList.add('active');
-
-        const dataFilter = this.getAttribute('data-filter')
-
-        for(let k=0; k<images.length;k++){
-            images[k].classList.remove('active');
-            images[k].classList.add('hide');
-
-            if(images[k].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
-                images[k].classList.remove('hide');
-                images[k].classList.add('active');
-            }
-        }
+types.forEach(function (type) {
+  type.addEventListener("click", function () {
+    types.forEach(function (type) {
+      type.classList.remove("active");
     })
-}
+    this.classList.add("active");
 
+    const dataFilter = this.getAttribute("data-filter");
 
+    images.forEach(function (image) {
+      image.classList.remove("active");
+      image.classList.add("hide");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      if (
+        image.getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        image.classList.remove("hide");
+        image.classList.add("active");
+      }
+    })
+  });
+})
